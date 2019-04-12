b85d586beb1f28d28a4d0d798712f2fd3731bc82
CJ Avilla <cjavilla@gmail.com>
2019-04-12 15:24:43 -0700

    Add Tests

--- spec/models/widget_spec.rb
+++ spec/models/widget_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Widget, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  xit "fails intentionally" do
+    expect(true).to be(false)
+  end
+
+  it "passes intentionally" do
+    expect(true).to be(true)
+  end
 end
